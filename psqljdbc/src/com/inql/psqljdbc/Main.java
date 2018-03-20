0381e4878cc0f05a2a4f33a6617493b23a511813
inql <dbinkus96@gmail.com>
2018-03-20 18:19:18 +0100

    Add logging to database with hidden password input v2

--- psqljdbc/src/com/inql/psqljdbc/Main.java
+++ psqljdbc/src/com/inql/psqljdbc/Main.java
@@ -1,7 +1,10 @@
 package com.inql.psqljdbc;
 
+import com.inql.psqljdbc.ui.DatabaseLogging;
+
 public class Main {
     public static void main(String args[]) {
-
+        DatabaseLogging databaseLogging = new DatabaseLogging();
+        databaseLogging.connectToDatabase();
     }
 }
