ea79856bf8de07f3e70bdb04024874333e055292
inql <dbinkus96@gmail.com>
2018-03-20 17:54:42 +0100

    Add logging to database with hidden password input

---
+++ psqljdbc/src/com/inql/psqljdbc/Main.java
@@ -0,0 +1,7 @@
+package com.inql.psqljdbc;
+
+public class Main {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ psqljdbc/src/com/inql/psqljdbc/ui/DatabaseLogging.java
@@ -0,0 +1,66 @@
+package com.inql.psqljdbc.ui;
+
+import java.io.Console;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+public class DatabaseLogging {
+
+    private Connection connection;
+    private final Console console = System.console();
+    private String dbUrl;
+    private String dbName;
+    private String dbUsername;
+    private String dbPassword;
+    private Scanner scanner;
+
+    public DatabaseLogging() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void connectToDatabase(){
+        System.out.println("Do you like to change default settings?\n\t(Y/N)");
+        if(scanner.next().toLowerCase().equals("y")){
+            //todo change settings
+            insertNewSettings();
+        }
+        else{
+            //todo use saved settings
+            useSavedSettings();
+        }
+        connect();
+    }
+
+    private void insertNewSettings(){
+        System.out.print("Enter database address: ");
+        dbUrl=scanner.next();
+        System.out.print("\nEnter database name: ");
+        dbName=scanner.next();
+        System.out.print("\nEnter username: ");
+        dbUsername=scanner.next();
+        dbPassword = new String(console.readPassword("\nEnter password: "));
+
+    }
+
+    private void useSavedSettings(){
+
+    }
+
+    private void connect(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager
+                    .getConnection("jdbc:postgresql://"+dbUrl+":5432/"+dbName,
+                            dbUsername, dbPassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+
+
+}
